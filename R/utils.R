c957465fac2f647a89f37b8faeda7d6bf1b1a0d5
Tyler Bradley <tyler.bradley@phila.gov>
2020-03-21 10:19:53 -0400

    adding utility function

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,37 @@
 
 
-
+read_flash_cards <- function(path){
+  attempt::stop_if(
+    file.exists(path),
+    .p = isFALSE,
+    msg = "file specified does not exist!"
+  )
+  
+  attempt::stop_if(
+    tools::file_ext(path) %in% c("xlsx", "rds", "csv"),
+    .p = isFALSE,
+    msg = "file specified must be a `.xlsx`, `.csv`, or `.rds`"
+  )
+  
+  dat_ext <- tools::file_ext(path)
+  
+  if (dat_ext == "xlsx"){
+    out <- readxl::read_excel(path)
+  } else if (dat_ext == "csv"){
+    out <- readr::read_csv(path)
+  } else if (dat_ext == "rds"){
+    out <- readr::read_rds(path)
+  }
+  
+  out <- valid_flash_cards(out)
+  return(out)
+}
 
 valid_flash_cards <- function(.data){
    attempt::stop_if(
      all(c("question", "answer") %in% colnames(.data)), 
      .p = isFALSE,
-    msg = "`.data` must contain the columns `question` and `answer`" 
+     msg = "`.data` must contain the columns `question` and `answer`" 
    )
   
   return(.data)
