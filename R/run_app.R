09455dddb48e534b833f613d5722e263a6943c89
Tyler Bradley <tyler.bradley@phila.gov>
2020-03-23 19:40:31 -0400

    making the main function a wrapper

--- R/run_app.R
+++ R/run_app.R
@@ -1,16 +1,30 @@
 #' Run the Shiny Application
-#'
-#' @param ... A series of options to be used inside the app.
+#' @rdname flash_cards
 #' @param .data a data.frame containing columns `question` and `answer`
 #' @param path the path to a `.xlsx`, `.csv`, `.rds` file containing a 
 #' data.frame with columns `question` and `answer`
+#' @param type the type of flash_cards to run. Can be either "shiny" or "addin"
+#' @param width the width of the rstudio modal dialog box (in pixels)
+#' @param hieght the height of the rstudio modal dialog box (in pixels)
 #' 
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 flash_cards <- function(
-  .data = NULL, path = NULL
+  .data = NULL, path = NULL, type = "shiny"
 ) {
+  type <- match.arg(type, c("shiny", "addin"))
+  
+  if (type == "shiny"){
+    flash_shiny(.data = .data, path = path)
+  } else if (type == "addin"){
+    flash_addin(.data = .data, path = path)
+  }
+}
+
+#' @rdname flash_cards
+#' @export
+flash_shiny <- function(.data = NULL, path = NULL){
   with_golem_options(
     app = shinyApp(
       ui = app_ui, 
@@ -20,14 +34,7 @@ flash_cards <- function(
   )
 }
 
-#' Run shinyFlash as an RStudio Addin
-#' 
-#' @param .data a data.frame containing columns `question` and `answer`
-#' @param path the path to a `.xlsx`, `.csv`, `.rds` file containing a 
-#' data.frame with columns `question` and `answer`
-#' @param width the width of the rstudio modal dialog box (in pixels)
-#' @param hieght the height of the rstudio modal dialog box (in pixels)
-#' 
+#' @rdname flash_cards
 #' @export
 flash_addin <- function(.data = NULL, path = NULL, width = 1000, height = 800){
   viewer = shiny::dialogViewer("shinyFlash", width = width, height = height)
