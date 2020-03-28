c4a47af06128c80dada6009b1585cfc22ef3a371
Tyler Bradley <tyler.bradley@phila.gov>
2020-03-28 10:08:31 -0400

    adding documentation for datasets

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' shinyFlash Datasets
+#' @name flash_data
+NULL
+
+#' Advanced R Flash Cards
+#' 
+#' Questions and Answers from the Advanced R book by Hadley Wickham 
+#' (\link{https://adv-r.hadley.nz/}). 
+#' @rdname flash_data
+"adv_r_deck"
+
+#' DRBC Flash Cards
+#' 
+#' Questions and Answers about the Delaware River Basin Commission (DRBC). 
+#' These questions and answers were amde from material taught by Carol Collier
+#' and John Yagecic at Drexel University in the Winter Quarter of 2020. 
+#' @rdname flash_data
+"drbc_deck"
