fe3dc858dc1695be27edaf70c9f37fa68c13cb45
Tyler Bradley <tyler.bradley@phila.gov>
2020-03-28 10:39:09 -0400

    added ability to specify columns to the read and validate functions. Current defaults fall back on original implementation

--- R/utils.R
+++ R/utils.R
@@ -2,23 +2,27 @@
 #' Read Flash card files
 #' 
 #' @param path the path to a file. Must have columns `question` and `answer`. 
+#' @param question the column name for the questions in the dataset
+#' @param answer the column name for the answers in the dataset
+#' @param clean logical; whether or not to clean the column names
 #' 
 #' @details function currently accepts .xlsx, .csv, and .rds
-read_flash_cards <- function(path){
+read_flash_cards <- function(path, question = question, answer = answer, clean = TRUE){
   attempt::stop_if(
     file.exists(path),
     .p = isFALSE,
     msg = "file specified does not exist!"
   )
-  
   attempt::stop_if(
     tools::file_ext(path) %in% c("xlsx", "rds", "csv"),
     .p = isFALSE,
     msg = "file specified must be a `.xlsx`, `.csv`, or `.rds`"
   )
   
-  dat_ext <- tools::file_ext(path)
+  question <- dplyr::enquo(question)
+  answer <- dplyr::enquo(answer)
   
+  dat_ext <- tools::file_ext(path)
   if (dat_ext == "xlsx"){
     out <- readxl::read_excel(path)
   } else if (dat_ext == "csv"){
@@ -26,36 +30,62 @@ read_flash_cards <- function(path){
   } else if (dat_ext == "rds"){
     out <- readr::read_rds(path)
   }
-  
-  out <- valid_flash_cards(out)
+
+  out <- valid_flash_cards(out, question = !!question, 
+                           answer = !!answer, clean = clean)
   return(out)
 }
 
 #' validate that flash cards have the required columns
 #' 
 #' @param .data a date frame with columns `question` and `answer`
+#' @param question the column name for the questions in the dataset
+#' @param answer the column name for the answers in the dataset
+#' @param clean logical; whether or not to clean the column names
+#' 
+#' @details
+#' Note: If clean = TRUE (the default) then the columns specified by the 
+#' question and answer arguments must be in their cleaned form (lowercase 
+#' snakecase)
 #' 
 #' @details 
 #' the column names are case insensitive
-valid_flash_cards <- function(.data){
-  .data <- janitor::clean_names(.data)
-   attempt::stop_if(
-     is_valid_flash_cards(.data), 
-     .p = isFALSE,
-     msg = "`.data` must contain the columns `question` and `answer`" 
-   )
+valid_flash_cards <- function(.data, question = question, 
+                              answer = answer, clean = TRUE){
+  
+  if (clean) .data <- janitor::clean_names(.data)
+  
+  question <- dplyr::enquo(question)
+  answer <- dplyr::enquo(answer)
+  question_name <- dplyr::quo_name(question)
+  answer_name <- dplyr::quo_name(answer)
+  
+  
+  attempt::stop_if(
+    is_valid_flash_cards(.data, question = !!question, answer = !!answer, clean = clean), 
+    .p = isFALSE,
+    msg = glue::glue("`.data` must contain the columns `{question_name}` and `{answer_name}`") 
+  )
   
   out <- .data %>% 
-    dplyr::select(question, answer) %>% 
+    dplyr::select(!!question, !!answer) %>% 
     dplyr::mutate_all(as.character)
   
   return(out)
 }
 
-is_valid_flash_cards <- function(.data){
+is_valid_flash_cards <- function(.data, question = question, 
+                                 answer = answer, clean = TRUE){
   if (!inherits(.data, "data.frame")) return(FALSE)
-  .data <- janitor::clean_names(.data)
-  if (all(c("question", "answer") %in% colnames(.data))) {
+  if (clean) .data <- janitor::clean_names(.data)
+  
+  question <- dplyr::enquo(question)
+  answer <- dplyr::enquo(answer)
+  
+  question_name <- dplyr::quo_name(question)
+  answer_name <- dplyr::quo_name(answer)
+  
+  if (all(c(question_name, answer_name) %in% colnames(.data))) {
     return(TRUE)
   } else {
     return(FALSE)
